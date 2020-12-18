1a16cfe30c32cd61036d399f5b797da2680dce43
Jbrak <joe@brakoniecki.com>
2020-12-18 13:17:53 +0000

    Added tweet() function

--- tweet.py
+++ tweet.py
@@ -1,4 +1,5 @@
 import json
+import tweepy
 
 with open('auth.json') as f:
   data = json.load(f)
@@ -8,5 +9,12 @@ consumer_secret = data["consumer_secret"]
 access_token = data["access_token"]
 access_token_secret = data["access_token_secret"]
 
-print(data)
-print(consumer_key,"\n", access_token)
+#print(data)
+#print(consumer_key,"\n", access_token)
+
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
+#api = tweepy.API(auth)
+
+def tweet():
+    api.update_status("Hello World")
