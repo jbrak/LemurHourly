d8ae8d2270f1cba92a8ef94d928e7a90ecc33145
Jbrak <joe@brakoniecki.com>
2021-04-30 16:11:05 +0100

    stuff

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import schedule
 from time import sleep,time
+from tweet import tweet
 
 time1 = time()
 def job():
     #put code to be run hourly in here
+    tweet()
     print("This code is working...")
     print(time1-time())
 
 
-schedule.every(1.5).hours.do(job)
+schedule.every(1.5).hour.do(job)
 
 while True:
     schedule.run_pending()
