bc711811f12baab9219da0a579c13466925a3fc0
jbrak <joe@brakoniecki.com>
2023-12-21 16:14:44 +0000

    fixed tweet function to work with twitter API V2

--- tweet.py
+++ tweet.py
@@ -10,17 +10,33 @@ consumer_key = data["consumer_key"]
 consumer_secret = data["consumer_secret"]
 access_token = data["access_token"]
 access_token_secret = data["access_token_secret"]
+bearer_token = data["bearer_token"]
 
 #print(data)
 #print(consumer_key,"\n", access_token)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+#Creating V1.1 API object
+auth = tweepy.OAuth1UserHandler(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret)
 api = tweepy.API(auth)
+
+#Creating V2 Client Object
+client = tweepy.Client(bearer_token,
+                    access_token=access_token,
+                    access_token_secret=access_token_secret,
+                    consumer_key=consumer_key,
+                    consumer_secret=consumer_secret)
 
 def tweet(accumulator = 0):
     try:
-        api.update_with_media(randomFile())
+        #Using V1.1 API to create media object
+        media = api.media_upload(randomFile())
+        #Using V2 API to tweet image
+        client.create_tweet(media_ids=[media.media_id_string])
+
     except Exception as e:
         sleep(300)
         accumulator += 1
